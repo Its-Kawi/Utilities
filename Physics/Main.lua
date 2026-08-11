00bd0c5b60426aefabd1570b352d5d91b7f2e5f3
Kawi <sk4ndi1488@gmail.com>
2026-08-11 03:26:35 +0300

    Update Main.lua

--- Physics/Main.lua
+++ Physics/Main.lua
@@ -35,7 +35,9 @@ local cfz = ncf()
 
 local ino = getfenv().isnetworkowner
 local ghp = getfenv().gethiddenproperty
-local util = (getfenv().getgenv or function() return _G end)().QKUtil or (function() local rf, IF = getfenv().readfile or getfenv().read_file, getfenv().isfile or getfenv().is_file return loadstring(rf and IF and IF("QUtil/Utility.lua") and rf("QUtil/Utility.lua") or game:HttpGet("https://raw.githubusercontent.com/Its-Kawi/Utilities/refs/heads/main/Utility/Main.lua"))() end)()
+
+local GITHUB = "https://raw.githubusercontent.com/Its-Kawi/"
+local util = (getfenv().getgenv or function() return _G end)().QKUtil or (function() local url = GITHUB .. "Utilities/refs/heads/main/Utility/Main.lua" local rf, IF = getfenv().readfile or getfenv().read_file, getfenv().isfile or getfenv().is_file return loadstring(rf and IF and IF("QUtil/Utility.lua") and rf("QUtil/Utility.lua") or getfenv().request and getfenv().request({ Url = url, Method = "GET" }).Body or game:HttpGet(url))() end)()
 
 if g[libN] then
 	return g[libN]
